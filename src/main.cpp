40f46229d5a6316424015dbc19867c5b31fd7192
Christophe Chapuis <chris.chapuis@gmail.com>
2023-03-04 11:00:20 +0000

    Fix appinfo.json and initial size
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,14 @@ int main(int argc, char **argv)
     QApplication app(argc, argv);
     QQuickStyle::setStyle("LuneOS");
     QQuickView view;
-    
+
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    // setup an initial size
+    QSize screenSize = QGuiApplication::primaryScreen()->size();
+    view.setWidth(screenSize.width());
+    view.setHeight(screenSize.height());
+
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + "/qml/Main.qml"));
     view.show();
